27bf1f44b62f380527a97074f52ef908192a8d2c
r-i-c-h <rich@richwerden.com>
2018-09-22 01:00:43 -0700

    Fixed a typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,9 @@ const IndexPage = () => (
     <h1>Ahoy Hoy!</h1>
     <p>Hey look - I'm a customized Gatsby site.</p>
     <p>
-      My name is Rich, aka,
-      <a href="https://github.com/r-i-c-h/NetlifyDeployFutz" target="_blank"> https://github.com/r-i-c-h/ </a>
+      My name is Rich, aka, <a href="https://github.com/r-i-c-h/" target="_blank">https://github.com/r-i-c-h/</a>
+    </p>
+    <p>Though if you really want to see a bit more about me, I'd recommend my website <a href="https://richwerden.com" target="_blank">https://richwerden.com</a>
     </p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
